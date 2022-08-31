317bd5fb4242398e5be341eb68c342bc83b92486
Maxwell Paulo <Maxwell@Maxwells-MacBook-Pro.local>
2022-08-31 18:20:19 -0300

    Iteration 5

--- src/components/CreditCard/index.js
+++ src/components/CreditCard/index.js
@@ -27,16 +27,24 @@ export function CreditCard(props) {
   let textColor = { color: `${props.color}` };
 
   return (
-    <div className={style.container}>
-      <img src={cardImg} alt="card img" />
-      <p>.... .... .... {finalNumber}</p>
-      <div>
-        <p>
-          {month}/{year}
-        </p>
-        <p>{props.bank}</p>
-        <p>{props.owner}</p>
+    <div className={style.container} style={bgcolor}>
+      <div className={style.imgDiv}>
+        <img className={style.containerImg} src={cardImg} alt="card img" />
       </div>
+      <p style={textColor} className={style.cardNumber}>
+        .... .... .... {finalNumber}
+      </p>
+      <div className={style.containerDiv}>
+        <p style={textColor} className={style.infosText}>
+          Expires {month}/{year}
+        </p>
+        <p style={textColor} className={style.infosText}>
+          {props.bank}
+        </p>
+      </div>
+      <p style={textColor} className={style.infosText}>
+        {props.owner}
+      </p>
     </div>
   );
 }
